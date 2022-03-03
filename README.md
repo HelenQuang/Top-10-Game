d2ae6378363dbd4d05dda4949bbea7f9832b991e
HelenQuang <qvha.97@gmail.com>
2022-03-03 23:03:33 +0200

    HTML, CSS

---
+++ README.md
@@ -0,0 +1,11 @@
+## Sortable List (Small Learning Project)
+
+Display a scrambled list that can be sorted with drag and drop
+
+## Project Specifications
+
+- Create an ordered list
+- Scramble list items randomly
+- Allow user to drag and drop an item to a different position
+- Button to check if items are in correct order
+- Show green for correct order and red for wrong order
---
+++ script.js
@@ -0,0 +1,51 @@
+const mostPopulated = [
+  "China",
+  "India",
+  "United States",
+  "Indonesia",
+  "Pakistan",
+  "Brazil",
+  "Nigeria",
+  "Bangladesh",
+  "Rusia",
+  "Mexico",
+];
+
+const mostSpokenLanguage = [
+  "English",
+  "Mandarin",
+  "Hindi",
+  "Spanish",
+  "French",
+  "Arabic",
+  "Bengali",
+  "Russian",
+  "Portugese",
+  "Indonesian",
+];
+
+const richestPeople = [
+  "Elon Musk",
+  "Bernard Arnault",
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffett",
+  "Larry Page",
+  "Larry Ellison",
+  "Sergey Brin",
+  "Mark Zuckerberg",
+  " Steve Ballmer",
+];
+
+const programmingLanguage = [
+  "JavaScript",
+  "Python",
+  "C/C++",
+  "JAVA",
+  "R Language",
+  "Kotlin",
+  "C#",
+  "PHP",
+  "Go",
+  "Scala",
+];
