f83f90390f8cfaa6c7895c8a49182dfd4c6534ed
HelenQuang <qvha.97@gmail.com>
2022-03-06 23:24:50 +0200

    Complete

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-## Sortable List (Small Learning Project)
+## Top 10 Game (Small Learning Project)
 
-Display a scrambled list that can be sorted with drag and drop
+Display a scrambled list that can be sorted with [drag and drop](https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API)
 
 ## Project Specifications
 
 - Create an ordered list
-- Scramble list items randomly
-- Allow user to drag and drop an item to a different position
+- Scramble list items randomly after page reload
+- Allow user to drag and drop an item to different position
 - Button to check if items are in correct order
 - Show green for correct order and red for wrong order
--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const mostPopulated = [
+const mostPopulatedCountries = document.getElementById(
+  "most-populated-countries"
+);
+const mostSpokenLanguages = document.getElementById("most-spoken-languages");
+const richestPeople = document.getElementById("richest-people");
+const mostProgrammingLanguages = document.getElementById(
+  "most-programming-languages"
+);
+const check1 = document.getElementById("check1");
+const check2 = document.getElementById("check2");
+const check3 = document.getElementById("check3");
+const check4 = document.getElementById("check4");
+
+const mostPopulatedArr = [
   "China",
   "India",
   "United States",
@@ -11,7 +24,7 @@ const mostPopulated = [
   "Mexico",
 ];
 
-const mostSpokenLanguage = [
+const mostSpokenLanguageArr = [
   "English",
   "Mandarin",
   "Hindi",
@@ -24,7 +37,7 @@ const mostSpokenLanguage = [
   "Indonesian",
 ];
 
-const richestPeople = [
+const richestPeopleArr = [
   "Elon Musk",
   "Bernard Arnault",
   "Jeff Bezos",
@@ -34,10 +47,10 @@ const richestPeople = [
   "Larry Ellison",
   "Sergey Brin",
   "Mark Zuckerberg",
-  " Steve Ballmer",
+  "Steve Ballmer",
 ];
 
-const programmingLanguage = [
+const programmingLanguageArr = [
   "JavaScript",
   "Python",
   "C/C++",
@@ -50,105 +63,240 @@ const programmingLanguage = [
   "Scala",
 ];
 
-// Store listitems
-const listItems = [];
+// Store list items
+const listItems1 = [];
+const listItems2 = [];
+const listItems3 = [];
+const listItems4 = [];
 
 let dragStartIndex;
 
-createList();
-
-// Insert list items into DOM
-function createList() {
-  [...richestPeople]
+//Create list item into DOM
+const createList1 = () => {
+  [...mostPopulatedArr]
     .map((a) => ({ value: a, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map((a) => a.value)
-    .forEach((person, index) => {
+    .forEach((item, index) => {
       const listItem = document.createElement("li");
 
       listItem.setAttribute("data-index", index);
 
       listItem.innerHTML = `
-        <span class="number">${index + 1}</span>
-        <div class="draggable" draggable="true">
-          <p class="person-name">${person}</p>
-          <i class="fas fa-grip-lines"></i>
-        </div>
-      `;
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true"> 
+      <p class="item-name">${item}</p>
+      <i class="fas fa-grip-lines"></i>
+    `;
 
-      listItems.push(listItem);
+      listItems1.push(listItem);
 
-      draggable_list.appendChild(listItem);
+      mostPopulatedCountries.appendChild(listItem);
     });
 
-  addEventListeners();
-}
+  const swapItems = (fromIndex, toIndex) => {
+    const item1 = listItems1[fromIndex].querySelector(".draggable");
+    const item2 = listItems1[toIndex].querySelector(".draggable");
 
-function dragStart() {
-  // console.log('Event: ', 'dragstart');
-  dragStartIndex = +this.closest("li").getAttribute("data-index");
-}
+    listItems1[fromIndex].appendChild(item2);
+    listItems1[toIndex].appendChild(item1);
+  };
 
-function dragEnter() {
-  // console.log('Event: ', 'dragenter');
-  this.classList.add("over");
-}
+  addEventListeners(swapItems);
 
-function dragLeave() {
-  // console.log('Event: ', 'dragleave');
-  this.classList.remove("over");
-}
+  const checkOrder = () => {
+    listItems1.forEach((listItem, index) => {
+      const itemName = listItem.querySelector(".draggable").innerText.trim();
 
-function dragOver(e) {
-  // console.log('Event: ', 'dragover');
-  e.preventDefault();
-}
+      if (itemName !== mostPopulatedArr[index]) {
+        listItem.classList.add("wrong");
+      } else {
+        listItem.classList.remove("wrong");
+        listItem.classList.add("right");
+      }
+    });
+  };
 
-function dragDrop() {
-  // console.log('Event: ', 'drop');
-  const dragEndIndex = +this.getAttribute("data-index");
-  swapItems(dragStartIndex, dragEndIndex);
+  check1.addEventListener("click", checkOrder);
+};
 
-  this.classList.remove("over");
-}
+const createList2 = () => {
+  [...mostSpokenLanguageArr]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((item, index) => {
+      const listItem = document.createElement("li");
 
-// Swap list items that are drag and drop
-function swapItems(fromIndex, toIndex) {
-  const itemOne = listItems[fromIndex].querySelector(".draggable");
-  const itemTwo = listItems[toIndex].querySelector(".draggable");
+      listItem.setAttribute("data-index", index);
 
-  listItems[fromIndex].appendChild(itemTwo);
-  listItems[toIndex].appendChild(itemOne);
-}
+      listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+      <p class="item-name">${item}</p>
+      <i class="fas fa-grip-lines"></i>
+    `;
 
-// Check the order of list items
-function checkOrder() {
-  listItems.forEach((listItem, index) => {
-    const personName = listItem.querySelector(".draggable").innerText.trim();
+      listItems2.push(listItem);
 
-    if (personName !== richestPeople[index]) {
-      listItem.classList.add("wrong");
-    } else {
-      listItem.classList.remove("wrong");
-      listItem.classList.add("right");
-    }
-  });
-}
+      mostSpokenLanguages.appendChild(listItem);
+    });
 
-function addEventListeners() {
+  const swapItems = (fromIndex, toIndex) => {
+    const item1 = listItems2[fromIndex].querySelector(".draggable");
+    const item2 = listItems2[toIndex].querySelector(".draggable");
+
+    listItems2[fromIndex].appendChild(item2);
+    listItems2[toIndex].appendChild(item1);
+  };
+
+  addEventListeners(swapItems);
+
+  const checkOrder = () => {
+    listItems2.forEach((listItem, index) => {
+      const itemName = listItem.querySelector(".draggable").innerText.trim();
+
+      if (itemName !== mostSpokenLanguageArr[index]) {
+        listItem.classList.add("wrong");
+      } else {
+        listItem.classList.remove("wrong");
+        listItem.classList.add("right");
+      }
+    });
+  };
+
+  check2.addEventListener("click", checkOrder);
+};
+
+const createList3 = () => {
+  [...richestPeopleArr]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((item, index) => {
+      const listItem = document.createElement("li");
+
+      listItem.setAttribute("data-index", index);
+
+      listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+      <p class="item-name">${item}</p>
+      <i class="fas fa-grip-lines"></i>
+    `;
+
+      listItems3.push(listItem);
+
+      richestPeople.appendChild(listItem);
+    });
+
+  const swapItems = (fromIndex, toIndex) => {
+    const item1 = listItems3[fromIndex].querySelector(".draggable");
+    const item2 = listItems3[toIndex].querySelector(".draggable");
+
+    listItems3[fromIndex].appendChild(item2);
+    listItems3[toIndex].appendChild(item1);
+  };
+
+  addEventListeners(swapItems);
+
+  const checkOrder = () => {
+    listItems3.forEach((listItem, index) => {
+      const itemName = listItem.querySelector(".draggable").innerText.trim();
+
+      if (itemName !== richestPeopleArr[index]) {
+        listItem.classList.add("wrong");
+      } else {
+        listItem.classList.remove("wrong");
+        listItem.classList.add("right");
+      }
+    });
+  };
+
+  check3.addEventListener("click", checkOrder);
+};
+
+const createList4 = () => {
+  [...programmingLanguageArr]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((item, index) => {
+      const listItem = document.createElement("li");
+
+      listItem.setAttribute("data-index", index);
+
+      listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+      <p class="item-name">${item}</p>
+      <i class="fas fa-grip-lines"></i>
+    `;
+
+      listItems4.push(listItem);
+
+      mostProgrammingLanguages.appendChild(listItem);
+    });
+
+  const swapItems = (fromIndex, toIndex) => {
+    const item1 = listItems4[fromIndex].querySelector(".draggable");
+    const item2 = listItems4[toIndex].querySelector(".draggable");
+
+    listItems4[fromIndex].appendChild(item2);
+    listItems4[toIndex].appendChild(item1);
+  };
+
+  addEventListeners(swapItems);
+
+  const checkOrder = () => {
+    listItems4.forEach((listItem, index) => {
+      const itemName = listItem.querySelector(".draggable").innerText.trim();
+
+      if (itemName !== programmingLanguageArr[index]) {
+        listItem.classList.add("wrong");
+      } else {
+        listItem.classList.remove("wrong");
+        listItem.classList.add("right");
+      }
+    });
+  };
+
+  check4.addEventListener("click", checkOrder);
+};
+
+//Add event listeners
+const addEventListeners = (swapItemFunction) => {
   const draggables = document.querySelectorAll(".draggable");
   const dragListItems = document.querySelectorAll(".draggable-list li");
 
   draggables.forEach((draggable) => {
-    draggable.addEventListener("dragstart", dragStart);
+    draggable.addEventListener("dragstart", () => {
+      dragStartIndex = +draggable.closest("li").getAttribute("data-index");
+    });
   });
 
   dragListItems.forEach((item) => {
-    item.addEventListener("dragover", dragOver);
-    item.addEventListener("drop", dragDrop);
-    item.addEventListener("dragenter", dragEnter);
-    item.addEventListener("dragleave", dragLeave);
-  });
-}
+    item.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
 
-check.addEventListener("click", checkOrder);
+    item.addEventListener("drop", () => {
+      const dragEndIndex = item.getAttribute("data-index");
+      swapItemFunction(dragStartIndex, dragEndIndex);
+      item.classList.remove("over");
+    });
+
+    item.addEventListener("dragenter", () => {
+      item.classList.add("over");
+    });
+
+    item.addEventListener("dragleave", () => {
+      item.classList.remove("over");
+    });
+  });
+};
+
+createList1();
+createList2();
+createList3();
+createList4();
